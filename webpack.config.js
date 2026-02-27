7af80357d6027305b7bb470cdf10b2cbfb0f0a01
5 <5@DESKTOP-OH25RQJ>
2026-02-27 17:04:07 +0300

    Configure Webpack to copy assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin'; 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,9 +22,7 @@ export default {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
+                use: { loader: 'babel-loader' },
             },
             {
                 test: /\.css$/,
@@ -32,22 +31,26 @@ export default {
             {
                 test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'assets/[name][hash][ext][query]', 
+                },
             },
         ],
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
+        new HtmlWebpackPlugin({ template: './index.html' }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public', to: '' },
+                { from: 'src/assets', to: 'assets' }, 
+            ],
         }),
     ],
     devServer: {
-        static: [
-            './dist',
-            './public',  
-        ],
+        static: ['./dist', './public'],
         port: 5173,
         hot: true,
-        historyApiFallback: true,
         open: true,
+        historyApiFallback: true,
     },
 };
